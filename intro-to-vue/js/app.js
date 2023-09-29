987dc57e8001214ab24006fbd670c0b1fff59b28
Beregszászi István <beistvan@gmail.com>
2023-09-29 19:46:17 +0200

    Change The Data In App.js File

--- intro-to-vue/js/app.js
+++ intro-to-vue/js/app.js
@@ -2,12 +2,12 @@ const app = new Vue({
   el: '#app',
   data: {
     username: 'CoderInTraining',
-    bio: 'Excited future software engineer.',
     newTweet: '',
     tweets: [
       'Started learning to code today. Wish me luck!',
       'Okay, I learned HTML, CSS, and JavaScript. But, how do I combine them together?? Send help.',
       'Today I start learning Vue. I got this.'
-    ]
+    ],
+    bio: 'Excited future front-end engineer.'
   }
 });
